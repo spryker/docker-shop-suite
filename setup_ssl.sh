03331b1d84a05be70f55ea94e0b32d53e04fb369
Konstantin Tishkin <konstantin.tishkin@spryker.com>
2018-12-16 11:59:48 +0000

    HTTPS and Redirect can be enabled/disabled for different vhosts

--- setup_ssl.sh
+++ setup_ssl.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Checking env variable
-test -z ${HTTPS_ON} && exit 0
-test ${HTTPS_ON} -eq 0 && exit 0
+ZED_HTTPS=0
+YVES_HTTPS=0
+GLUE_HTTPS=0
+
+test -n "${ZED_HTTPS_ON}" && test "${ZED_HTTPS_ON}" -eq 1 && ZED_HTTPS=1
+test -n "${YVES_HTTPS_ON}" && test "${YVES_HTTPS_ON}" -eq 1 && YVES_HTTPS=1
+test -n "${GLUE_HTTPS_ON}" && test "${GLUE_HTTPS_ON}" -eq 1 && GLUE_HTTPS=1
 
 # Checking the first input parameter with the domain
 mainDomain=$1
@@ -12,63 +16,112 @@ test -z ${mainDomain} && echo "No domain specified. Exiting" && exit 1
 myIp=$2
 test -z ${myIp} && echo "No IP specified. Exiting" && exit 1
 
-# Concatinating all Spryker domains
-myDomains="${mainDomain} www.${mainDomain} glue.${mainDomain} os.${mainDomain}"
-
 # Install Dig if it doesn't installed yet
-test -z "$(which dig)" && (apt-get update && apt-get install -y dnsutils)
+function checkDig(){
+  test -z "$(which dig)" && (apt-get update && apt-get install -y dnsutils)
+}
 
 # Function which return the IP address of the domain name input as the first parameter of the function
-function resolve_domain(){
+function resolveDomain(){
   domain=$1
-  test -z ${domain} && echo "No domain specified as the parameter of the resolve_domain function. Exiting" && exit 1
+  test -z ${domain} && echo "No domain specified as the parameter of the resolveDomain function. Exiting" && exit 1
   ip=$(dig ${domain} a +short|grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"|head -n 1)
   echo ${ip}
 }
 
-# Checking for DNS records set
-for domain in $(echo $myDomains); do
+#Checking that domain resolves with correct IP 
+function checkDomain(){
+  domain=$1
+  checkDig
   echo -n "Resolving ${domain}, got \""
-  domainIp=$(resolve_domain ${domain})
+  domainIp=$(resolveDomain ${domain})
   echo "${domainIp}\""
 
-  # Check for DNS changes every 20 sec
   while [ "${domainIp}" != "${myIp}" ]; do
-      echo "Waiting for DNS changes for ${domain}. It should point to ${myIp}"
-      sleep 20
-      domainIp=$(resolve_domain ${domain})
+    echo "Waiting for DNS changes for ${domain}. It should point to ${myIp}"
+    sleep 20
+    domainIp=$(resolveDomain ${domain})
   done
   echo "OK"
-done
+}
 
-echo "All DNS records exist and are correct"
+function getCertificates(){
+  domains="$1"
+  cd /tmp
+  repoDir=/tmp/certbot
+  # Get certbot sources
+  if [ -d ${repoDir}/.git ]; then
+      cd ${repoDir}
+      git pull
+  else
+      git clone https://github.com/certbot/certbot.git ${repoDir}
+  fi
 
-cd /tmp
+  echo  ${repoDir}/letsencrypt-auto certonly --webroot -w /usr/share/nginx/html -d $(echo "${domains}"|sed "s/ / -d /g") -m admin@${mainDomain} --agree-tos -n --expand
+}
 
-repoDir=/tmp/certbot
-# Get certbot sources
-if [ -d ${repoDir}/.git ]; then
-    cd ${repoDir}
-    git pull
-else
-    git clone https://github.com/certbot/certbot.git ${repoDir}
+function checkCertificates(){
+  domain=$1
+  ls -la /etc/letsencrypt/live/${domain}/fullchain.pem
+  ls -la /etc/letsencrypt/live/${domain}/privkey.pem
+  
+  # Check if certificate and key was generated and put it in the vhosts configs
+  if [ -L /etc/letsencrypt/live/${domain}/fullchain.pem -a -L /etc/letsencrypt/live/${domain}/privkey.pem ]; then
+      fullchain=$(readlink -f /etc/letsencrypt/live/${domain}/fullchain.pem)
+      privkey=$(readlink -f /etc/letsencrypt/live/${domain}/privkey.pem)
+      if [ -f ${fullchain} -a -f ${privkey} ]; then
+        echo OK
+      fi
+  fi
+}
+
+# Processing ZED domain(s)
+if [ ${ZED_HTTPS} -eq 1 ];then
+  myDomains="os.${mainDomain}"
+  checkDig
+  for domain in $(echo ${myDomains}); do
+    checkDomain ${domain}
+  done
+
+  getCertificates "${myDomains}"
+
+  if [ "$(checkCertificates ${mainDomain})" == "OK" ]; then
+    echo j2 /etc/nginx/conf.d/ssl/ssl.vhost-zed.conf.j2 > /etc/nginx/conf.d/vhost-zed.conf
+  else
+    echo FAIL
+  fi
 fi
 
-${repoDir}/letsencrypt-auto certonly --webroot -w /usr/share/nginx/html -d $(echo ${myDomains}|sed "s/ / -d /g") -m admin@${mainDomain} --agree-tos -n --expand
-
-
-ls -la /etc/letsencrypt/live/${mainDomain}/fullchain.pem
-ls -la /etc/letsencrypt/live/${mainDomain}/privkey.pem
-
-# Check if certificate and key was generated and put it in the vhosts configs
-if [ -L /etc/letsencrypt/live/${mainDomain}/fullchain.pem -a -L /etc/letsencrypt/live/${mainDomain}/privkey.pem ]; then
-    fullchain=$(readlink -f /etc/letsencrypt/live/${mainDomain}/fullchain.pem)
-    privkey=$(readlink -f /etc/letsencrypt/live/${mainDomain}/privkey.pem)
-    if [ -f ${fullchain} -a -f ${privkey} ]; then
-        j2 /etc/nginx/conf.d/ssl/ssl.vhost-yves.conf.j2 > /etc/nginx/conf.d/vhost-yves.conf
-        j2 /etc/nginx/conf.d/ssl/ssl.vhost-zed.conf.j2 > /etc/nginx/conf.d/vhost-zed.conf
-        #j2 /etc/nginx/conf.d/ssl/ssl.vhost-glue.conf.j2 > /etc/nginx/conf.d/vhost-glue.conf
-
-	supervisorctl restart nginx
-    fi
+# Processing YVES domain(s)
+if [ ${YVES_HTTPS} -eq 1 ];then
+  myDomains="${mainDomain} www.${mainDomain}"
+  checkDig
+  for domain in $(echo ${myDomains}); do
+    checkDomain ${domain}
+  done
+  getCertificates "${myDomains}"
+  if [ "$(checkCertificates ${mainDomain})" == "OK" ]; then
+    echo j2 /etc/nginx/conf.d/ssl/ssl.vhost-yves.conf.j2 > /etc/nginx/conf.d/vhost-yves.conf
+  else
+    echo FAIL
+  fi
 fi
+
+# Processing GLUE domain(s)
+if [ ${GLUE_HTTPS} -eq 1 ];then
+  myDomains="glue.${mainDomain}"
+  checkDig
+  for domain in $(echo ${myDomains}); do
+    checkDomain ${domain}
+  done
+
+  getCertificates "${myDomains}"
+
+  if [ "$(checkCertificates ${mainDomain})" == "OK" ]; then
+    echo j2 /etc/nginx/conf.d/ssl/ssl.vhost-glue.conf.j2 > /etc/nginx/conf.d/vhost-glue.conf
+  else
+    echo FAIL
+  fi
+fi
+
+supervisorctl restart nginx
